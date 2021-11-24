469d451f24da99a2aee8d0b4601501d532a46868
intrager <intrager@naver.com>
2021-11-24 15:43:16 +0900

    비디오 탭 및 기능 추가

---
+++ bmiCalcHJS/bmiCalcHJS/bmiCalcHJS/WebViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WebViewController.swift
+//  bmiCalcHJS
+//
+//  Created by 소프트웨어컴퓨터 on 2021/11/24.
+//  Copyright © 2021 kr.ac.induk.hjs. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
